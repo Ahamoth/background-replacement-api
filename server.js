6b4a6304573a0fa526dce2495cd08f3959cbf56f
Andrey <109838774+Ahamoth@users.noreply.github.com>
2025-11-25 23:54:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.post('/generate', upload.fields([
     console.log('🎯 Качество:', quality);
 
     const API_KEY = process.env.GEMINI_API_KEY || "AIzaSyCkeDnvwpoxquYUKxCCXHO87UQO0U8ICwc";
-    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-3-pro-image-preview:generateContent?key=${API_KEY}`;
 
     const defaultPrompt = `
 Create a photorealistic composite by perfectly integrating the object from the first image 
